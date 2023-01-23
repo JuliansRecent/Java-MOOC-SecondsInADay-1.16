a5132761e5fa61db1974631ad21ec7a12636485c
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 11:38:52 -0800

    Module complete. Prompt the user for the amount of days to convert to seconds, convert the inputted amount of days to seconds. Output the amount of seconds.

--- src/main/java/SecondsInADay.java
+++ src/main/java/SecondsInADay.java
@@ -7,6 +7,13 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        // Prompt the user for the amount of days they would like to convert to0 seconds
+        System.out.println("How many days would you like to convert to seconds?");
+        // Store the amount of days the user inputted
+        int numberOfDays = Integer.valueOf(scanner.nextLine());
+        // Convert the amount of days inputted to seconds in the numberOfSeconds variable
+        int numberOfSeconds = numberOfDays * 86400;
+        // Output the numberOfSeconds variable
+        System.out.println(numberOfSeconds);
     }
 }
